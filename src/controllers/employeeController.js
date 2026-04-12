87785dd7f01be0a131c919e2a78d37f55d73985f
prashant2150487 <swepra04@gmail.com>
2026-04-12 10:20:01 +0530

    fix: remove error message from employee not found response in employeeController

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -130,7 +130,6 @@ export const updateEmployeeById = async (req, res) => {
             return res.status(404).json({
                 message: "Employee not found",
                 sucess: false,
-                error: err.message
 
             })
         }
